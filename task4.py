9f10f071e52edf5ed67a4429e7de6286503a2ff7
Samy Boutouis <sb590@duke.edu>
2023-04-21 17:24:35 -0400

    implement approach 2

--- task4.py
+++ task4.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from task3 import dtw
 
-def pairwise_centering(trajectories):
+def approach_1(trajectories):
     table = [[0 for j in range(len(trajectories))] for i in range(len(trajectories))]
     for i in range(len(trajectories)):
         for j in range(len(trajectories)):
@@ -20,6 +20,34 @@ def pairwise_centering(trajectories):
             min_trajectory = trajectory
     
     return trajectories[min_trajectory]
+
+def approach_2(trajectories):
+    # Initialize an empty list to store the average trajectory
+    avg_trajectory = []
+    
+    # Initialize a list to keep track of the number of updates for each point in the average trajectory
+    point_counts = []
+    
+    # Iterate over all the trajectories
+    for t in trajectories:
+        # Iterate over all the points in the current trajectory
+        for i, p in enumerate(t):
+            # If the current index is greater than or equal to the length of the average trajectory,
+            # append a new point to the average trajectory with the same coordinates as the current point
+            if i >= len(avg_trajectory):
+                avg_trajectory.append(p)
+                point_counts.append(1)
+            # Otherwise, update the i-th point in the average trajectory by computing the weighted average
+            # of its current coordinates and the coordinates of the current point, and increasing the count
+            # of updates for the point by 1
+            else:
+                count = point_counts[i]
+                avg_trajectory[i] = [((count * avg_trajectory[i][0]) + p[0]) / (count + 1), ((count * avg_trajectory[i][1]) + p[1]) / (count + 1)]
+                point_counts[i] += 1
+    
+    # Return the average trajectory as the center trajectory
+    return avg_trajectory
+
 
 def main():
     with open('trajectory-ids.txt', 'r') as file:
@@ -47,10 +75,15 @@ def main():
         plt.ylabel('Y in km')
         plt.plot(x_trajectory, y_trajectory, color='black', marker='.', markersize=8)
     
-    # pairwise_center = pairwise_centering(t)
-    # x_trajectory = [point[0] for point in pairwise_center]
-    # y_trajectory = [point[1] for point in pairwise_center]
+    # approach_1_center = approach_1(t)
+    # x_trajectory = [point[0] for point in approach_1_center]
+    # y_trajectory = [point[1] for point in approach_1_center]
     # plt.plot(x_trajectory, y_trajectory, color='red', linestyle='dashed', marker='.', markersize=8)
+
+    approach_2_center = approach_2(t)
+    x_trajectory = [point[0] for point in approach_2_center]
+    y_trajectory = [point[1] for point in approach_2_center]
+    plt.plot(x_trajectory, y_trajectory, color='red', linestyle='dashed', marker='.', markersize=8)
 
     plt.show()
 
